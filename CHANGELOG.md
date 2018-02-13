e620af3b780c7cddbbca22f4d32ebd4af6d553e5
Roman Roelofsen <romanroe@gmail.com>
2018-02-13 13:50:19 +0100

    - Requires RxJS 5.5.6 - added `untilComponentDestroyed` method which can be used in the `.pipe()` operator

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+
+# Version 2.0.0
+
+- Requires RxJS 5.5.6
+- added `untilComponentDestroyed` method which can be used in the `.pipe()` operator
+
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,24 @@
-import {Subject} from "rxjs/Subject";
 import {Observable} from "rxjs/Observable";
+import {takeUntil} from "rxjs/operators";
+import {Subject} from "rxjs/Subject";
 
 export type OnDestroyLike = {
     ngOnDestroy(): void;
 }
 
-export function componentDestroyed(component: OnDestroyLike): Observable<undefined> {
+export function componentDestroyed(component: OnDestroyLike): Observable<true> {
     const oldNgOnDestroy = component.ngOnDestroy;
-    const stop$ = new Subject();
+    const stop$ = new Subject<true>();
     component.ngOnDestroy = function () {
-        oldNgOnDestroy && oldNgOnDestroy.apply(component);
-        stop$.next(undefined);
+        oldNgOnDestroy && oldNgOnDestroy.apply(component, arguments);
+        stop$.next(true);
         stop$.complete();
     };
-    return stop$;
+    return stop$.asObservable();
 }
 
+export function untilComponentDestroyed<T>(this: Observable<any>, component: OnDestroyLike): Observable<T> {
+    return this.pipe(
+        takeUntil(componentDestroyed(component))
+    );
+}

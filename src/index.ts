ed9d9199c15afa2f224f6f5f1e70fe613fb473c5
Michael Klein <mischkl@users.noreply.github.com>
2018-02-21 13:47:52 +0100

    Reinstate type, remove unnecessary apply arguments
    Type-check on component is needed to ensure it has an ngOnDestroy() method.
    ngOnDestroy doesn't take any arguments, therefore they are unnecessary.

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,24 @@ import {Observable} from "rxjs/Observable";
 import {takeUntil} from "rxjs/operators";
 import {ReplaySubject} from "rxjs/ReplaySubject";
 
-export function componentDestroyed(component: any): Observable<true> {
+export type OnDestroyLike = {
+    ngOnDestroy(): void;
+}
+
+export function componentDestroyed(component: OnDestroyLike): Observable<true> {
     if (component.__componentDestroyed$) {
         return component.__componentDestroyed$;
     }
     const oldNgOnDestroy = component.ngOnDestroy;
     const stop$ = new ReplaySubject<true>();
     component.ngOnDestroy = function () {
-        oldNgOnDestroy && oldNgOnDestroy.apply(component, arguments);
+        oldNgOnDestroy && oldNgOnDestroy.apply(component);
         stop$.next(true);
         stop$.complete();
     };
     return component.__componentDestroyed$ = stop$.asObservable();
 }
 
-export function untilComponentDestroyed<T>(component: any): (source: Observable<T>) => Observable<T> {
+export function untilComponentDestroyed<T>(component: OnDestroyLike): (source: Observable<T>) => Observable<T> {
     return (source: Observable<T>) => source.pipe(takeUntil(componentDestroyed(component)));
 }
